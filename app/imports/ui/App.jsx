f3821da603cd7455eb5bcbcfe0674108ec6d0ae9
aldenparoni <aparoni@hawaii.edu>
2024-01-17 09:41:43 -1000

    personal review

---
+++ app/imports/ui/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+
+export const App = () => (
+  <div>
+    <h1>ICS 414 is extremely stressful!!</h1>
+    <Hello/>
+    <Info/>
+  </div>
+);
